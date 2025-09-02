92f8253c169c662e8d2c0a786ceaa0a5ab33f44b
Pratik Kumar Sahoo <pratiksahoo591@gmail.com>
2025-09-02 08:17:34 +0530

    8th

---
+++ execv.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<unistd.h>
+main(){
+    char *temp[3];
+    temp[0]="ls";
+    temp[1]="-l";
+    temp[2]=(char *)0;
+    execv("/bin/ls",temp);
+    printf("this will not print\n");
+}
