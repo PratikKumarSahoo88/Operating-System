688c2ca85ab55fdde43d0d06caec440de169dd0d
Pratik Kumar Sahoo <pratiksahoo591@gmail.com>
2025-09-02 09:55:15 +0530

    12th

---
+++ pipe.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<unistd.h>
+#include<sys/ipc.h>
+#include<sys/types.h>
+#define msgsize 12
+void main()
+{
+    char*msg="hello world";
+    char inbuff[msgsize];
+    int p[2],pid,j;
+    pipe(p);
+    pid=fork();
+    if(pid>0){
+        close(p[0]);
+        write(p[1],msg,msgsize);
+    }
+    if(pid==0){
+        close(p[1]);
+        read(p[0],inbuff,msgsize);
+        printf("%s \n",inbuff);
+    }
+}
