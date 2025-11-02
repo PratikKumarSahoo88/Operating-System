cea121d4a0a331ec599ea5edff1f3bb0bee74ea8
Pratik Kumar Sahoo <pratiksahoo591@gmail.com>
2025-11-02 19:25:17 +0530

    Implement LRU page replacement algorithm

---
+++ LRU.c
@@ -0,0 +1,142 @@
+#include <stdio.h>
+
+
+
+int findLRU(int time[], int n) {
+
+    int min = time[0], pos = 0;
+
+    for (int i = 1; i < n; i++) {
+
+        if (time[i]<min) {
+
+            min = time[i];
+
+            pos = i;
+
+        }
+
+    }
+
+    return pos;
+
+}
+
+
+
+int main() {
+
+    int frames[10], pages[30], time[10];
+
+    int n, f, pageFaults = 0, counter = 0;
+
+
+
+    printf("Enter number of frames: ");
+
+    scanf("%d",&f);
+
+
+
+    printf("Enter number of pages: ");
+
+    scanf("%d",&n);
+
+
+
+    printf("Enter reference string:\n");
+
+    for (int i = 0; i < n; i++)
+
+        scanf("%d",&pages[i]);
+
+
+
+    for (int i = 0; i < f; i++)
+
+        frames[i] = -1;
+
+
+
+    for (int i = 0; i < n; i++) {
+
+        int flag1 = 0, flag2 = 0;
+
+        for (int j = 0; j<f; j++) {
+
+            if (frames[j] == pages[i]) {
+
+                counter++;
+
+                time[j] = counter;
+
+                flag1 = flag2 = 1;
+
+                break;
+
+            }
+
+        }
+
+        if (!flag1) {
+
+            for (int j = 0; j<f; j++) {
+
+                if (frames[j] == -1) {
+
+                    counter++;
+
+                    frames[j] = pages[i];
+
+                    time[j] = counter;
+
+                    pageFaults++;
+
+                    flag2 = 1;
+
+                    break;
+
+                }
+
+            }
+
+        }
+
+        if (!flag2) {
+
+            int pos = findLRU(time, f);
+
+            counter++;
+
+            frames[pos] = pages[i];
+
+            time[pos] = counter;
+
+            pageFaults++;
+
+        }
+
+
+
+        printf("\nFor page %d : ", pages[i]);
+
+        for (int j = 0; j<f; j++)
+
+            if (frames[j] != -1)
+
+                printf("%d ", frames[j]);
+
+            else
+
+                printf("- ");
+
+    }
+
+
+
+    printf("\nTotal Page Faults = %d\n", pageFaults);
+
+    return 0;
+
+}
+
